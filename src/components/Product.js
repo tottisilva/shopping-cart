95cc0d081ebc17dac621a2fb9f9dabefd7d9ad6a
Jose Silva <73293985+tottisilva@users.noreply.github.com>
2020-12-12 16:16:05 +0000

    Add files via upload

---
+++ src/components/Product.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function Product(props) {
+    const {product, onAdd} = props;
+    return (
+        <div>
+           <img className="small" src={product.image} alt={product.name}></img> 
+           <h3>{product.name}</h3>
+           <div>€{product.price}</div>
+           <button onClick={()=> onAdd(product)}>Add to Cart</button>
+        </div>
+    )
+}
---
+++ src/data.js
@@ -0,0 +1,23 @@
+const data ={
+    products: [
+    {
+        id: '1',
+        name:' Mackbook Air',
+        price: 1300,
+        image:' https://i.picsum.photos/id/0/5616/3744.jpg?hmac=3GAAioiQziMGEtLbfrdbcoenXoWAW-zlyEAMkfEdBzQ'
+    },
+    {
+        id: '2',
+        name: 'Mackbook Pro',
+        price: 1500,
+        image: 'https://i.picsum.photos/id/119/3264/2176.jpg?hmac=PYRYBOGQhlUm6wS94EkpN8dTIC7-2GniC3pqOt6CpNU'
+    },
+    {
+        id: '3',
+        name: 'Mackbook Air 13',
+        price: 1100,
+        image: 'https://i.picsum.photos/id/180/2400/1600.jpg?hmac=Ig-CXcpNdmh51k3kXpNqNqcDYTwXCIaonYiBOnLXBb8'
+    },
+],
+};
+export default data;
